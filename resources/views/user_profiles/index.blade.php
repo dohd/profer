c4e4af8d369c3e5c1a386f3b07ab208aa4d0242a
Dohd <alloicebrian@gmail.com>
2023-02-05 21:01:46 +0300

    Rename username

--- resources/views/user_profiles/index.blade.php
+++ resources/views/user_profiles/index.blade.php
@@ -22,7 +22,7 @@
           <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
 
             <img src="{{ asset('img/profile-img.jpeg') }}" alt="Profile" class="rounded-circle">
-            <h2>Supper Admin</h2>
+            <h2>Super Admin</h2>
             <h3>Web Designer</h3>
             <div class="social-links mt-2">
               <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
