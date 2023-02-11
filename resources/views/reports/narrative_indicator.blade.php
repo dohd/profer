95e36dc2ad74f51336adbd15cf60180735f3957c
dohd <alloicebrian@gmail.com>
2023-02-11 20:42:11 +0300

    Add proposal to narrative indicator

--- resources/views/reports/narrative_indicator.blade.php
+++ resources/views/reports/narrative_indicator.blade.php
@@ -8,11 +8,21 @@
         <div class="card-body">
             <div class="card-content p2">
                 <div class="row">
+                    <div class="col-12">
+                        <select name="proposal_id" id="proposal" class="form-select mt-3">
+                            <option value="">-- Choose Project --</option>
+                            @foreach ($proposals as $item)
+                                <option value="{{ $item->id }}">{{ $item->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col-4">
                         <select name="narrative_pointer_id" id="indicator" class="form-select mt-3">
                             <option value="">-- Choose Narrative No. --</option>
                             @foreach (range(1,5) as $item)
-                                <option value="{{ $item }}">{{ $item }}/2023 </option>
+                                <option value="{{ $item }}">{{ $item }}/{{ $item }}/2023 </option>
                             @endforeach
                         </select>
                     </div>
@@ -58,26 +68,18 @@
     <div class="card">
         <div class="card-body">
             <div class="card-content p-2">
-                {{-- <table class="table table-borderless" id="indicator_narrative_tbl">
-                    <thead>
-                      <tr>
-                        <th scope="col">#No</th>
-                        <th scope="col" width="15%">Date</th>
-                        <th scope="col">Programme</th>
-                        <th scope="col">Activity</th>
-                        <th scope="col">Region</th>
-                        <th scope="col">Cohort</th>
-                        <th scope="col" width="12%">Ps. Count</th>
-                        <th scope="col">Response</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                        
-                    </tbody>
-                  </table> --}}
+                <table class="table table-bordered mt-3">
+                    <tr><th width="20%">Project</th> <td class="project"></td></tr>
+                    <tr><th width="20%">Narrative No.</th> <td class="narrative_no"></td></tr>
+                    <tr><th width="20%">Programme</th> <td class="programme"></td></tr>
+                    <tr><th width="20%">Region</th> <td class="region"></td></tr>
+                    <tr><th width="20%">Cohort</th> <td class="cohort"></td></tr>
+                </table>
+                <h5 class="narrative_ind">Narrative Indicator: </h5>
+                <p class="response"></p>
             </div>
         </div>
-    </div>
+    </div> 
 @stop
 
 @section('script')
