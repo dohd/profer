b7fe12843bee3056582df04e0e57468732d2d699
dohd <alloicebrian@gmail.com>
2023-02-11 20:39:35 +0300

    Add utility model trait

---
+++ app/Models/ModelTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+trait ModelTrait
+{
+    /**
+     * @return string
+     */
+    public function getViewButtonAttribute($route, $permission='')
+    {
+        return '<a class="dropdown-item pt-1 pb-1 view" href="'. route($route, $this). '"><i class="bi bi-eye-fill"></i>View</a>';
+    }
+
+    /**
+     * @return string
+     */
+    public function getEditButtonAttribute($route, $permission='')
+    {
+        return '<a class="dropdown-item pt-1 pb-1 edit" href="'. route($route, $this) . '"><i class="bi bi-pencil-square"></i>Edit</a>';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeleteButtonAttribute($route, $permission='')
+    {
+        return '<a class="dropdown-item pt-1 pb-1 destroy" href="javascript:">
+                <i class="bi bi-trash text-danger icon-xs"></i>Delete
+                <form action="'. route($route, $this) .'" method="DELETE">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="hidden" name="_token" value="'. csrf_token() .'">
+                </form>
+            </a>';
+    }
+
+    /**
+     * @return string
+     */
+    public function getButtonWrapperAttribute($view, $edit, $delete)
+    {
+        return '<div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Action
+            </button>
+            <ul class="dropdown-menu">
+                <li>'. $view .'</li>
+                <li>'. $edit .'</li>
+                <li>'. $delete .'</li>
+            </ul>
+        </div>
+        ';
+    }
+}
