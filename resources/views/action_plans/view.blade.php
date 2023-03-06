249a63ccd6fa0076f738d60acb243ffdec34c84a
dohd <alloicebrian@gmail.com>
2023-03-06 17:34:20 +0300

    Add tab persistance on action_plans

--- resources/views/action_plans/view.blade.php
+++ resources/views/action_plans/view.blade.php
@@ -56,7 +56,7 @@
             
             <ul class="nav nav-tabs" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Activity</button>
+                    <button class="nav-link" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Activity</button>
                 </li>
                 <li class="nav-item" role="presentation">
                     <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Target Cohort</button>
@@ -169,6 +169,13 @@
             // });
         });
     });
-    
+
+    // persist selected tab
+    $('.nav-link').on('show.bs.tab', function() {
+        localStorage['activeTab'] = $(this).attr('data-bs-target');
+    });
+    const activeTab = localStorage['activeTab']
+    if (activeTab) $('button[data-bs-target="'+ activeTab +'"]').addClass('active').click(); 
+    else $('.nav-link:first').addClass('active');
 </script>
 @stop
