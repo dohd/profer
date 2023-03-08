83d634b60f8fe69b55fc8131f37ed3c96ef21cf1
dohd <alloicebrian@gmail.com>
2023-03-08 09:06:32 +0300

    Rename breadcrumb item

--- resources/views/action_plans/header.blade.php
+++ resources/views/action_plans/header.blade.php
@@ -3,7 +3,7 @@
   <nav>
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
-      <li class="breadcrumb-item active"><a href="{{ route('action_plans.index') }}">Action Plans</a></li>
+      <li class="breadcrumb-item active"><a href="{{ route('action_plans.index') }}">Action Plan</a></li>
     </ol>
   </nav>
 </div>
