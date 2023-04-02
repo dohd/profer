9294a0cb9e536d1c77f31813b026df6d8a815203
dohd <alloicebrian@gmail.com>
2023-04-02 12:46:22 +0300

    Update sidebar menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,383 +10,146 @@
 
     <li class="nav-heading">Key Indicators</li>
 
-    {{-- donor --}}
     <li class="nav-item">
       <a
         class="nav-link collapsed"
-        data-bs-target="#donor-nav"
+        data-bs-target="#key-indicator"
         data-bs-toggle="collapse"
         href="#"
       >
-        <i class="bi bi-tag"></i><span>Donor</span
+        <i class="bi bi-tag"></i><span>Key Indicators</span
         ><i class="bi bi-chevron-down ms-auto"></i>
       </a>
       <ul
-        id="donor-nav"
+        id="key-indicator"
         class="nav-content collapse"
         data-bs-parent="#sidebar-nav"
       >
         <li>
-          <a href="{{ route('donors.create') }}"><i class="bi bi-circle"></i><span>Create Donor</span></a>
+          <a href="{{ route('donors.index') }}"><i class="bi bi-circle"></i><span>Donors</span></a>
         </li>
         <li>
-          <a href="{{ route('donors.index') }}"><i class="bi bi-circle"></i><span>Manage Donor</span></a>
+          <a href="{{ route('programmes.index') }}"><i class="bi bi-circle"></i><span>Key Programmes</span></a>
+        </li>
+        <li>
+          <a href="{{ route('regions.index') }}"><i class="bi bi-circle"></i><span>Target Regions</span></a>
+        </li>
+        <li>
+          <a href="{{ route('cohorts.index') }}"><i class="bi bi-circle"></i><span>Target Cohorts</span></a>
+        </li>
+        <li>
+          <a href="{{ route('age_groups.index') }}"><i class="bi bi-circle"></i><span>Age Groups</span></a>
+        </li>
+        <li>
+          <a href="{{ route('disabilities.index') }}"><i class="bi bi-circle"></i><span>Disability Types</span></a>
         </li>
       </ul>
     </li>
 
-    {{-- programme --}}
+    <li class="nav-heading">Project Management</li>
+
     <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#programme-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-file-earmark-text"></i><span>Programme</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
+      <a class="nav-link collapsed" href="{{ route('proposals.index') }}">
+        <i class="bi bi-journal-text"></i></i><span>Grant Proposals</span>
       </a>
-      <ul
-        id="programme-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('programmes.create') }}"><i class="bi bi-circle"></i><span>Create Programme</span></a>
-        </li>
-        <li>
-          <a href="{{ route('programmes.index') }}"><i class="bi bi-circle"></i><span>Manage Programmes</span></a>
-        </li>
-      </ul>
     </li>
 
-    {{-- region --}}
+    {{-- projects --}}
     <li class="nav-item">
       <a
         class="nav-link collapsed"
-        data-bs-target="#region-nav"
+        data-bs-target="#project-nav"
         data-bs-toggle="collapse"
         href="#"
       >
-        <i class="bi bi-geo-alt-fill"></i><span>Target Region</span
+        <i class="bi bi-kanban"></i><span>Projects</span
         ><i class="bi bi-chevron-down ms-auto"></i>
       </a>
       <ul
-        id="region-nav"
+        id="project-nav"
         class="nav-content collapse"
         data-bs-parent="#sidebar-nav"
       >
         <li>
-          <a href="{{ route('regions.create') }}"><i class="bi bi-circle"></i><span>Create Region</span></a>
+          <a href="{{ route('proposals.index') }}"><i class="bi bi-circle"></i><span>Manage Projects</span></a>
         </li>
         <li>
-          <a href="{{ route('regions.index') }}"><i class="bi bi-circle"></i><span>Manage Regions</span></a>
-        </li>
-      </ul>
-    </li>
-
-    {{-- target cohort --}}
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#cohort-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-people"></i><span>Target Cohort</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul
-        id="cohort-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('cohorts.create') }}"><i class="bi bi-circle"></i><span>Create Cohort</span></a>
-        </li>
-        <li>
-          <a href="{{ route('cohorts.index') }}"><i class="bi bi-circle"></i><span>Manage Cohorts</span></a>
-        </li>
-      </ul>
-    </li>
-
-    {{-- disabiliy --}}
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#disability-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-ticket-perforated"></i><span>Disability</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul
-        id="disability-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('disabilities.create') }}"><i class="bi bi-circle"></i><span>Create Disability</span></a>
-        </li>
-        <li>
-          <a href="{{ route('disabilities.index') }}"><i class="bi bi-circle"></i><span>Manage Disabilities</span></a>
-        </li>
-      </ul>
-    </li>
-
-    {{-- age group --}}
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#agegroup-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-sort-down-alt"></i><span>Age Group</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul
-        id="agegroup-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('age_groups.create') }}"><i class="bi bi-circle"></i><span>Create Age Group</span></a>
-        </li>
-        <li>
-          <a href="{{ route('age_groups.index') }}"><i class="bi bi-circle"></i><span>Manage Age Groups</span></a>
-        </li>
-      </ul>
-    </li>
-
-    <li class="nav-heading">Programme Management</li>
-
-    {{-- proposal --}}
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#proposals-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-kanban"></i><span>Grant Proposal</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul
-        id="proposals-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('proposals.create') }}"><i class="bi bi-circle"></i><span>Create Proposal</span></a>
-        </li>
-        <li>
-          <a href="{{ route('proposals.index') }}"><i class="bi bi-circle"></i><span>Manage Proposals</span></a>
-        </li>
-      </ul>
-    </li>
-
-    {{-- log frame --}}
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#logframe-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-journal-text"></i><span>Log Frame</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul
-        id="logframe-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('log_frames.create') }}"><i class="bi bi-circle"></i><span>Create Log Frame</span></a>
-        </li>
-        <li>
-          <a href="{{ route('log_frames.index') }}"><i class="bi bi-circle"></i><span>Manage Log Frames</span></a>
+          <a href="{{ route('log_frames.index') }}"><i class="bi bi-circle"></i><span>Project Log Frame</span></a>
         </li>
       </ul>
     </li>
 
     {{-- action plan --}}
     <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#action-plan-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-calendar-event"></i><span>Action Plan</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
+      <a class="nav-link collapsed" href="{{ route('action_plans.index') }}">
+        <i class="bi bi-calendar-event"></i><span>Action Plan</span>
       </a>
-      <ul
-        id="action-plan-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('action_plans.create') }}"><i class="bi bi-circle"></i><span>Create Plan</span></a>
-        </li>
-        <li>
-          <a href="{{ route('action_plans.index') }}"><i class="bi bi-circle"></i><span>Manage Plans</span></a>
-        </li>
-      </ul>
     </li>
 
-    {{-- activity implementation --}}
+    {{-- participant list --}}
     <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#act-implementation-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-clipboard-data"></i><span>Participant List</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
+      <a class="nav-link collapsed" href="{{ route('participant_lists.index') }}">
+        <i class="bi bi-clipboard-data"></i></i><span>Participant List</span>
       </a>
-      <ul
-        id="act-implementation-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ route('participant_lists.create') }}"><i class="bi bi-circle"></i><span>Create Participant List</span></a>
-        </li>
-        <li>
-          <a href="{{ route('participant_lists.index') }}"><i class="bi bi-circle"></i><span>Manage Participant Lists</span></a>
-        </li>
-      </ul>
     </li>
 
     {{-- activity narrative --}}
     <li class="nav-item">
+      <a class="nav-link collapsed" href="{{ route('narratives.index') }}">
+        <i class="bi bi-journal-text"></i></i><span>Activity Narrative</span>
+      </a>
+    </li>
+
+    {{-- reports --}}
+    <li class="nav-heading">Reports</li>
+    <li class="nav-item">
       <a
         class="nav-link collapsed"
-        data-bs-target="#act-narrative-nav"
+        data-bs-target="#report"
         data-bs-toggle="collapse"
         href="#"
       >
-        <i class="bi bi-journal-text"></i><span>Activity Narrative</span
+        <i class="bi bi-card-list"></i><span>Reports</span
         ><i class="bi bi-chevron-down ms-auto"></i>
       </a>
       <ul
-        id="act-narrative-nav"
+        id="report"
+        class="nav-content collapse"
+        data-bs-parent="#sidebar-nav"
+      >
+      <li>
+        <a href="{{ route('reports.narrative_indicator') }}"><i class="bi bi-circle"></i><span>Narrative Report</span></a>
+      </li>
+      <li>
+        <a href="{{ route('reports.participant_analysis') }}"><i class="bi bi-circle"></i><span>Activity Report</span></a>
+      </li>
+      </ul>
+    </li>
+
+    {{-- user management --}}
+    <li class="nav-heading">User Management</li>
+    <li class="nav-item">
+      <a
+        class="nav-link collapsed"
+        data-bs-target="#user-management"
+        data-bs-toggle="collapse"
+        href="#"
+      >
+        <i class="bi bi-people"></i><span>User Management</span
+        ><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul
+        id="user-management"
         class="nav-content collapse"
         data-bs-parent="#sidebar-nav"
       >
         <li>
-          <a href="{{ route('narratives.create') }}"><i class="bi bi-circle"></i><span>Create Narrative</span></a>
+          <a href="{{ '#' }}"><i class="bi bi-circle"></i><span>Roles & Rights</span></a>
         </li>
         <li>
-          <a href="{{ route('narratives.index') }}"><i class="bi bi-circle"></i><span>Manage Narratives</span></a>
+          <a href="{{ '#' }}"><i class="bi bi-circle"></i><span>Users</span></a>
         </li>
       </ul>
     </li>
-
-    <li class="nav-heading">Input/Output Analysis</li>
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="{{ route('reports.narrative_indicator') }}">
-        <i class="bi bi-card-list"></i>
-        <span>Narrative Indicator</span>
-      </a>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="{{ route('reports.participant_analysis') }}">
-        <i class="bi bi-card-list"></i>
-        <span>Participant Analysis</span>
-      </a>
-    </li>
-
-    <li class="nav-heading">Authorization Control</li>
-
-    {{-- roles --}}
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#account-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-journal-text"></i><span>Roles</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul
-        id="account-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ '#' }}"><i class="bi bi-circle"></i><span>Create Role</span></a>
-        </li>
-        <li>
-          <a href="{{ '#' }}"><i class="bi bi-circle"></i><span>Manage Roles</span></a>
-        </li>
-      </ul>
-    </li>
-
-    {{-- users --}}
-    <li class="nav-item">
-      <a
-        class="nav-link collapsed"
-        data-bs-target="#users-nav"
-        data-bs-toggle="collapse"
-        href="#"
-      >
-        <i class="bi bi-people"></i><span>Users</span
-        ><i class="bi bi-chevron-down ms-auto"></i>
-      </a>
-      <ul
-        id="users-nav"
-        class="nav-content collapse"
-        data-bs-parent="#sidebar-nav"
-      >
-        <li>
-          <a href="{{ '#' }}"><i class="bi bi-circle"></i><span>Create User</span></a>
-        </li>
-        <li>
-          <a href="{{ '#' }}"><i class="bi bi-circle"></i><span>Manage Users</span></a>
-        </li>
-      </ul>
-    </li>
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="{{ route('user_profiles.index') }}">
-        <i class="bi bi-person"></i>
-        <span>Profile</span>
-      </a>
-    </li>
- {{--    
-    <li class="nav-heading">Pages</li>
-    
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="{{ route('register') }}">
-        <i class="bi bi-card-list"></i>
-        <span>Register</span>
-      </a>
-    </li>
-    <!-- End Register Page Nav -->
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="{{ route('login') }}">
-        <i class="bi bi-box-arrow-in-right"></i>
-        <span>Login</span>
-      </a>
-    </li>
-    <!-- End Login Page Nav -->
-
-    <li class="nav-item">
-      <a class="nav-link collapsed" href="{{ '#' }}">
-        <i class="bi bi-dash-circle"></i>
-        <span>Error 404</span>
-      </a>
-    </li>
-    <!-- End Error 404 Page Nav -->
-     --}}
   </ul>
 </aside>
