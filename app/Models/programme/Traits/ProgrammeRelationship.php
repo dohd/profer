a63dd2ff48d03c5d728c40bae148d007bf21fbb6
dohd <alloicebrian@gmail.com>
2023-04-18 11:59:20 +0300

    Remove participant relation

--- app/Models/programme/Traits/ProgrammeRelationship.php
+++ app/Models/programme/Traits/ProgrammeRelationship.php
@@ -3,17 +3,11 @@
 namespace App\Models\programme\Traits;
 
 use App\Models\action_plan\ActionPlan;
-use App\Models\participant_list\ParticipantList;
 
 trait ProgrammeRelationship
 {
     public function action_plans()
     {
         return $this->hasMany(ActionPlan::class);
-    }
-
-    public function participant_lists()
-    {
-        return $this->hasMany(ParticipantList::class);
     }
 }
