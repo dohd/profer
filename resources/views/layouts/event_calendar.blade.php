dd59f0c000c88c07847b1c63c7c0bb85fbd7cb01
dohd <alloicebrian@gmail.com>
2023-04-27 17:21:29 +0300

    Update event calendar js

--- resources/views/layouts/event_calendar.blade.php
+++ resources/views/layouts/event_calendar.blade.php
@@ -10,36 +10,34 @@
                 <div class="row">
                     <div class="col-12">
                         <div id="eventCalendar"></div>
-                        <script>            
-                            let stateCheck = setInterval(() => {
-                                if (document.readyState === 'complete') {
-                                    clearInterval(stateCheck);
-                                    // document ready
-                                    let events = @json($events);
-                                    events = events.map(v => ({
-                                        start: v.start_date,
-                                        end: v.end_date,
-                                        title: v.activity.name || '',
-                                        startEditable: false,
-                                    }));
-                                    // console.log(events)
-                                    
-                                    let ec = new EventCalendar(document.getElementById('eventCalendar'), {
-                                        events,
-                                        view: 'dayGridMonth',
-                                        headerToolbar: {
-                                            start: 'prev,next today',
-                                            center: 'title',
-                                            end: 'dayGridMonth,listWeek'
-                                        },
-                                        eventTimeFormat: time => '',
-                                    }); 
-                                }           
-                            }, 100);                    
-                        </script>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@stop
+
+@section('script')
+<script>    
+    $(() => {
+        let events = @json($events);
+        events = events.map(v => ({
+            start: v.start_date,
+            end: v.end_date,
+            title: v.activity.name || '',
+            startEditable: false,
+        }));
+        
+        let ec = new EventCalendar($('#eventCalendar')[0], {
+            events,
+            view: 'dayGridMonth',
+            headerToolbar: {
+                start: 'prev,next today',
+                center: 'title',
+                end: 'dayGridMonth,listWeek'
+            },
+            eventTimeFormat: time => '',
+        }); 
+    });                  
+</script>
 @stop
