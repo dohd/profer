ea94a7550b173f9a15c75e08bac4dd23375ea0bd
Dohd <alloicebrian@gmail.com>
2023-05-07 13:39:55 +0300

    Update participant list header

--- resources/views/participant_lists/header.blade.php
+++ resources/views/participant_lists/header.blade.php
@@ -1,11 +1,11 @@
 <div class="pagetitle">
-  <div class="row">
-    <h1 class="col-sm-10 col-xs-12">Participant List Management</h1>
-    <div class="col-sm-2 col-xs-12">
+  <h1>
+    Participant List Management
+    <div class="float-end">
       <a href="{{ route('participant_lists.index') }}" class="btn btn-secondary"><i class="bi bi-card-list"></i> List</a>
       <a href="{{ route('participant_lists.create') }}" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Create</a>
     </div>
-  </div>
+  </h1>
   <nav>
     <ol class="breadcrumb">
       <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
