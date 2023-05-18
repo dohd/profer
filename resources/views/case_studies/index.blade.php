bc51a68641a3b29510348e67c265be1dcf04763b
Dohd <alloicebrian@gmail.com>
2023-05-18 22:34:05 +0300

    Add programme to case study index page

--- resources/views/case_studies/index.blade.php
+++ resources/views/case_studies/index.blade.php
@@ -10,19 +10,21 @@
                 <div class="table-responsive">
                     <table class="table table-borderless datatable">
                         <thead>
-                        <tr>
-                            <th scope="col">#No.</th>
-                            <th scope="col">#Code</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Action</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">#No.</th>
+                                <th scope="col">#Code</th>
+                                <th scope="col">Programme</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Action</th>
+                            </tr>
                         </thead>
                         <tbody>
                             @foreach ($case_studies as $i => $item)
                                 <tr>
                                     <th scope="row">{{ $i+1 }}</th>
                                     <td><a href="{{ route('case_studies.show', $item) }}">{{ tidCode('case_study', $item->tid) }}</a></td>
+                                    <td>{{ @$item->programme->name }}</td>
                                     <td>{{ $item->title }}</td>
                                     <td>{{ dateFormat($item->date) }}</td>
                                     <td>{!! $item->action_buttons !!}</td>
